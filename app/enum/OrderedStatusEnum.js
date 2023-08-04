b1d90f2c98941c8d4326ca0c6c27091eb59c76f5
vikanshu-98 <vikanshu8954985003@mail.com>
2023-08-04 19:55:54 +0530

    update enum for order status

---
+++ app/enum/OrderedStatusEnum.js
@@ -0,0 +1,27 @@
+class OrderedStatusEnum{ 
+    static ORDER_RECEIVED   = 'order_placed'
+    static ORDER_CONFIRMED  =  'order_confirmed'
+    static IN_THE_KITCHEN   = 'in_the_kitchen'
+    static BEGIN_PACKED     = 'begin_packed'
+    static OUT_FOR_DELIVERY = 'out_for_delivery'
+    static ORDER_COMPLTED  = 'delivered'
+
+
+    static UI_ORDER_RECEIVED   = 'Order Received'
+    static UI_ORDER_CONFIRMED  =  'Order Confirmed'
+    static UI_IN_THE_KITCHEN   = 'In the Kitchen'
+    static UI_BEGIN_PACKED     = 'Begin Packed'
+    static UI_OUT_FOR_DELIVERY = 'Out For Delivery'
+    static UI_ORDER_COMPLTED  = 'Order completed'
+   
+   
+    static  1 = 'order_placed'
+    static  2 = 'order_confirmed'
+    static  3 = 'in_the_kitchen'
+    static  4 = 'begin_packed'
+    static  5 = 'out_for_delivery'
+    static  6 = 'delivered' 
+    
+}
+
+export default OrderedStatusEnum
